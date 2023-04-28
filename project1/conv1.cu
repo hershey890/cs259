b32306d86594538effaeb46df775a3bbb2fbac39
eriadnus <rickyrguo@g.ucla.edu>
2023-04-28 14:30:08 -0700

    fix dimensionality

--- project1/conv1.cu
+++ project1/conv1.cu
@@ -22,11 +22,11 @@ using namespace std;
 #define CONV1_Ty  8
 #define CONV1_Tx  8
 
-#define CONV1_NYPAD (CONV1_Ny+CONV1_Ky)
-#define CONV1_NXPAD (CONV1_Nx+CONV1_Kx)
+#define CONV1_NYPAD (CONV1_Ny) // #define CONV1_NYPAD (CONV1_Ny+CONV1_Ky)
+#define CONV1_NXPAD (CONV1_Nx) // #define CONV1_NXPAD (CONV1_Nx+CONV1_Kx)
 
-#define CONV1_NYSCL (CONV1_Ny/CONV1_Sy)
-#define CONV1_NXSCL (CONV1_Nx/CONV1_Sx)
+#define CONV1_NYSCL ((CONV1_Ny - CONV1_Ky + 1)/CONV1_Sy) // #define CONV1_NYSCL (CONV1_Ny/CONV1_Sy)
+#define CONV1_NXSCL ((CONV1_Nx - CONV1_Kx + 1)/CONV1_Sx) // #define CONV1_NXSCL (CONV1_Nx/CONV1_Sx)
 
 #define CONV1_SYNAPSE_SIZE (1L*CONV1_Ky*CONV1_Kx*CONV1_Nn*CONV1_Ni)
 
@@ -39,13 +39,13 @@ VTYPE (*neuron_n2)[NYSCL][NXSCL][Nn]; // Output Channel 2
 void convolution_layer_base(VTYPE (&synapse)[CONV1_Ky][CONV1_Kx][CONV1_Nn][CONV1_Ni],
                               VTYPE (&neuron_i)[CONV1_NYPAD][CONV1_NXPAD][CONV1_Ni],
                               VTYPE (&neuron_n)[CONV1_NYSCL][CONV1_NXSCL][CONV1_Nn]) {
-    for (int nx = 0; nx < CONV1_Nx; nx += CONV1_Sx) {
-        for (int ny = 0; ny < CONV1_Ny; ny += CONV1_Sy) {
-            for (int kx = 0; kx < CONV1_Kx; kx++) {
-                for (int ky = 0; ky < CONV1_Ky; ky++) {
+    for (int ny = 0; ny + CONV1_Ky < CONV1_Ny; ny += CONV1_Sy) {
+        for (int nx = 0; nx CONV1_Kx < CONV1_Nx; nx += CONV1_Sx) {
+            for (int ky = 0; ky < CONV1_Ky; ky++) {
+                for (int kx = 0; kx < CONV1_Kx; kx++) {
                     for (int ni = 0; ni < Ni; ni++) {
                         for (int nn = 0; nn < Nn; nn++) {
-                            neuron_n[nx][ny][nn] += neuron_i[nx + kx][ny + ky][ni] * synapse[ky][kx][nn][ni];
+                            neuron_n[ny][nx][nn] += neuron_i[ny + ky][nx + kx][ni] * synapse[ky][kx][nn][ni];
                         }
                     }
                 }
