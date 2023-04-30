dc9cd06f20d63e80726bcaf5c49a6377b174a12a
Ricky Guo rickyrguo@g.ucla.edu <eriadnus@tetracosa.cs.ucla.edu>
2023-04-29 18:53:31 -0700

    conv1 hits 8ms for large dataset

--- project1/conv1.cu
+++ project1/conv1.cu
@@ -1,5 +1,14 @@
-#include <iostream>
 #include <string>
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
+#include <cassert>
+#include "cuda_profiler_api.h"
+#include <cuda_runtime.h>
+#include <cuda.h>
+#include "cublas_v2.h"
+#include "../cuda_common.h"
+
 
 using namespace std;
 
@@ -24,8 +33,8 @@ using namespace std;
 #define CONV1_NYPAD (CONV1_Ny) // #define CONV1_NYPAD (CONV1_Ny+CONV1_Ky)
 #define CONV1_NXPAD (CONV1_Nx) // #define CONV1_NXPAD (CONV1_Nx+CONV1_Kx)
 
-#define CONV1_NYSCL ((CONV1_Ny - CONV1_Ky + 1)/CONV1_Sy) // #define CONV1_NYSCL (CONV1_Ny/CONV1_Sy)
-#define CONV1_NXSCL ((CONV1_Nx - CONV1_Kx + 1)/CONV1_Sx) // #define CONV1_NXSCL (CONV1_Nx/CONV1_Sx)
+#define CONV1_NYSCL (CONV1_Ny - CONV1_Ky + 1) // #define CONV1_NYSCL (CONV1_Ny/CONV1_Sy)
+#define CONV1_NXSCL (CONV1_Nx - CONV1_Kx + 1) // #define CONV1_NXSCL (CONV1_Nx/CONV1_Sx)
 
 #define CONV1_SYNAPSE_SIZE (CONV1_Ky*CONV1_Kx*CONV1_Nn*CONV1_Ni)
 #define CONV1_NEURON_INPUT_SIZE (CONV1_NYPAD*CONV1_NXPAD*CONV1_Ni)
@@ -34,7 +43,7 @@ using namespace std;
 #define CONV1_THREADS 1024
 #define CONV1_BLOCKS 500
 
-#define CONV1_ROWS_PROCESSED ((CONV1_NYSCL)/(CONV1_THREADS*CONV1_BLOCKS*CONV1_Sy))
+#define CONV1_ROWS_PROCESSED ((CONV1_Ny-CONV1_Ky)/(CONV1_THREADS*CONV1_BLOCKS))
 // #define CONV1_COLS_PROCESSED ((CONV1_NXSCL)/(CONV1_THREADS*CONV1_BLOCKS))
 
 using VTYPE = float;
@@ -73,7 +82,7 @@ __global__ void convolution_layer_base_gpu(VTYPE (&synapse)[CONV1_Ky][CONV1_Kx][
     int x = blockIdx.x * blockDim.x + threadIdx.x;
 
     const int start_row = x * CONV1_ROWS_PROCESSED;
-    const int end_row = min(CONV1_NYSCL, start_row + CONV1_ROWS_PROCESSED);
+    const int end_row = min(CONV1_Ky, start_row + CONV1_ROWS_PROCESSED);
     for (int ny = start_row; ny < end_row; ny += CONV1_Sy) {
         for (int nx = 0; nx + CONV1_Kx < CONV1_Nx; nx += CONV1_Sx) {
             for (int ky = 0; ky < CONV1_Ky; ky++) {
@@ -89,50 +98,94 @@ __global__ void convolution_layer_base_gpu(VTYPE (&synapse)[CONV1_Ky][CONV1_Kx][
     }
 }
 
-// GPU Version (unrolled kernel + re-shaped data layouts for input and synapse)
-__global__ void convolution_layer_tiled_gpu(VTYPE (&synapse_2)[CONV1_Ni][CONV1_Nn][CONV1_Ky][CONV1_Kx],
-                                           VTYPE (&neuron_i_2)[CONV1_Ni][CONV1_NYPAD][CONV1_NXPAD],
-                                           VTYPE (&neuron_n)[CONV1_NYSCL][CONV1_NXSCL][CONV1_Nn]) {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
 
-    const int start_row = x * CONV1_ROWS_PROCESSED;
-    const int end_row = min(CONV1_NYSCL, start_row + CONV1_ROWS_PROCESSED);
-    for (int ny = start_row; ny < end_row; ny += CONV1_Sy) {
-        for (int nx = 0; nx + CONV1_Kx < CONV1_Nx; nx += CONV1_Sx) {
-            for (int ni = 0; ni < CONV1_Ni; ni++) {
-                for (int nn = 0; nn < CONV1_Nn; nn++) {
-                    neuron_n[ny][nx][nn] = (
-                            neuron_i_2[ni][ny+0][nx+0] * synapse_2[ni][nn][0][0]
-                            + neuron_i_2[ni][ny+0][nx+1] * synapse_2[ni][nn][0][1]
-                            + neuron_i_2[ni][ny+0][nx+2] * synapse_2[ni][nn][0][2]
-                            + neuron_i_2[ni][ny+1][nx+0] * synapse_2[ni][nn][1][0]
-                            + neuron_i_2[ni][ny+1][nx+1] * synapse_2[ni][nn][1][1]
-                            + neuron_i_2[ni][ny+1][nx+2] * synapse_2[ni][nn][1][2]
-                            + neuron_i_2[ni][ny+2][nx+0] * synapse_2[ni][nn][2][0]
-                            + neuron_i_2[ni][ny+2][nx+1] * synapse_2[ni][nn][2][1]
-                            + neuron_i_2[ni][ny+2][nx+2] * synapse_2[ni][nn][2][2]
-                    );
-                }
-            }
-        }
-    }
+#define NEUR_OUT_ADDR(ny, nx, nn) (CONV1_Nx*CONV1_Nn*(ny) + CONV1_Nn*(nx) + (nn))
+#define NEUR_IN_ADDR(ni, ny, nx) (CONV1_Ny*CONV1_Nx*(ni) + CONV1_Nx*(ny) + (nx))
+#define SYNAPSE_ADDR(ni, nn, ky, kx) (CONV1_Nn * CONV1_Ky*CONV1_Kx*(ni) + CONV1_Ky*CONV1_Kx*(nn) + CONV1_Kx*(ky) + (kx))
+
+
+// GPU Version (unrolled kernel + re-shaped data layouts for input and synapse)
+// __global__ void convolution_layer_tiled_gpu(VTYPE synapse_2[CONV1_Ni][CONV1_Nn][CONV1_Ky][CONV1_Kx],
+//                                            VTYPE neuron_i_2[CONV1_Ni][CONV1_NYPAD][CONV1_NXPAD],
+//                                            VTYPE neuron_n[CONV1_NYSCL][CONV1_NXSCL][CONV1_Nn]) {
+__global__ void convolution_layer_tiled_gpu(VTYPE *synapse_2, VTYPE *neuron_i_2, VTYPE *neuron_n) {
+
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int z = blockIdx.z * blockDim.z + threadIdx.z;
+
+    
+    // int x = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // const int start_row = x * CONV1_ROWS_PROCESSED;
+    // const int end_row = min(CONV1_Ky, start_row + CONV1_ROWS_PROCESSED);
+    // for (int ny = start_row; ny < end_row; ny += CONV1_Sy) {
+    //     for (int nx = 0; nx + CONV1_Kx < CONV1_Nx; nx += CONV1_Sx) {
+    //         for (int ni = 0; ni < CONV1_Ni; ni++) {
+    //             for (int nn = 0; nn < CONV1_Nn; nn++) {
+    //                 neuron_n[NEUR_OUT_ADDR(ny, nx, nn)] = 
+    //                       neuron_i_2[NEUR_IN_ADDR(ni, ny+0, nx+0)] * synapse_2[SYNAPSE_ADDR(ni, nn, 0, 0)]
+    //                     + neuron_i_2[NEUR_IN_ADDR(ni, ny+0, nx+1)] * synapse_2[SYNAPSE_ADDR(ni, nn, 0, 1)]
+    //                     + neuron_i_2[NEUR_IN_ADDR(ni, ny+0, nx+2)] * synapse_2[SYNAPSE_ADDR(ni, nn, 0, 2)]
+    //                     + neuron_i_2[NEUR_IN_ADDR(ni, ny+1, nx+0)] * synapse_2[SYNAPSE_ADDR(ni, nn, 1, 0)]
+    //                     + neuron_i_2[NEUR_IN_ADDR(ni, ny+1, nx+1)] * synapse_2[SYNAPSE_ADDR(ni, nn, 1, 1)]
+    //                     + neuron_i_2[NEUR_IN_ADDR(ni, ny+1, nx+2)] * synapse_2[SYNAPSE_ADDR(ni, nn, 1, 2)]
+    //                     + neuron_i_2[NEUR_IN_ADDR(ni, ny+2, nx+0)] * synapse_2[SYNAPSE_ADDR(ni, nn, 2, 0)]
+    //                     + neuron_i_2[NEUR_IN_ADDR(ni, ny+2, nx+1)] * synapse_2[SYNAPSE_ADDR(ni, nn, 2, 1)]
+    //                     + neuron_i_2[NEUR_IN_ADDR(ni, ny+2, nx+2)] * synapse_2[SYNAPSE_ADDR(ni, nn, 2, 2)];
+
+    //                 // neuron_n[ny][nx][nn] = (
+    //                 //           neuron_i_2[ni][ny+0][nx+0] * synapse_2[ni][nn][0][0]
+    //                 //         + neuron_i_2[ni][ny+0][nx+1] * synapse_2[ni][nn][0][1]
+    //                 //         + neuron_i_2[ni][ny+0][nx+2] * synapse_2[ni][nn][0][2]
+    //                 //         + neuron_i_2[ni][ny+1][nx+0] * synapse_2[ni][nn][1][0]
+    //                 //         + neuron_i_2[ni][ny+1][nx+1] * synapse_2[ni][nn][1][1]
+    //                 //         + neuron_i_2[ni][ny+1][nx+2] * synapse_2[ni][nn][1][2]
+    //                 //         + neuron_i_2[ni][ny+2][nx+0] * synapse_2[ni][nn][2][0]
+    //                 //         + neuron_i_2[ni][ny+2][nx+1] * synapse_2[ni][nn][2][1]
+    //                 //         + neuron_i_2[ni][ny+2][nx+2] * synapse_2[ni][nn][2][2]
+    //                 // );
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 int main(const int argc, const char** argv) {
+    VTYPE *synapse_2 =  (VTYPE*)malloc(sizeof(VTYPE) * CONV1_SYNAPSE_SIZE);
+    VTYPE *neuron_i_2 = (VTYPE*)malloc(sizeof(VTYPE) * CONV1_NEURON_INPUT_SIZE);
+    VTYPE *neuron_n =   (VTYPE*)malloc(sizeof(VTYPE) * CONV1_NEURON_OUTPUT_SIZE);
+
 //     synapse   = (VTYPE (*)[CONV1_Ky][CONV1_Kx][CONV1_Nn][CONV1_Ni])  malloc(CONV1_SYNAPSE_SIZE*sizeof(VTYPE));
 //     neuron_i  = (VTYPE (*)[CONV1_NYPAD][CONV1_NXPAD][CONV1_Ni])malloc(CONV1_NYPAD*CONV1_NXPAD*CONV1_Ni*sizeof(VTYPE))
-    synapse_2   = (VTYPE (*)[CONV1_Ni][CONV1_Nn][CONV1_Ky][CONV1_Kx])  malloc(CONV1_SYNAPSE_SIZE*sizeof(VTYPE));
-    neuron_i_2  = (VTYPE (*)[CONV1_Ni][CONV1_NYPAD][CONV1_NXPAD])malloc(CONV1_NYPAD*CONV1_NXPAD*CONV1_Ni*sizeof(VTYPE));
-    neuron_n  = (VTYPE (*)[CONV1_NYSCL][CONV1_NXSCL][CONV1_Nn])malloc(CONV1_NYSCL*CONV1_NXSCL*CONV1_Nn*sizeof(VTYPE));
+    // synapse_2   = (VTYPE (*)[CONV1_Ni][CONV1_Nn][CONV1_Ky][CONV1_Kx])  malloc(CONV1_SYNAPSE_SIZE*sizeof(VTYPE));
+    // neuron_i_2  = (VTYPE (*)[CONV1_Ni][CONV1_NYPAD][CONV1_NXPAD])malloc(CONV1_NYPAD*CONV1_NXPAD*CONV1_Ni*sizeof(VTYPE));
+    // neuron_n  = (VTYPE (*)[CONV1_NYSCL][CONV1_NXSCL][CONV1_Nn])malloc(CONV1_NYSCL*CONV1_NXSCL*CONV1_Nn*sizeof(VTYPE));
     // neuron_n2 = (VTYPE (*)[CONV1_NYSCL][CONV1_NXSCL][CONV1_Nn])aligned_malloc(64,CONV1_NYSCL*CONV1_NXSCL*CONV1_Nn*sizeof(VTYPE));
-    float *cuInput, *cuKernels, *cuOutput;
-    cudaMalloc(&cuInput, CONV1_NEURON_INPUT_SIZE*sizeof(VTYPE));
-    cudaMalloc(&cuKernels, CONV1_SYNAPSE_SIZE*sizeof(VTYPE));
-    cudaMalloc(&cuOutput, CONV1_NEURON_OUTPUT_SIZE*sizeof(VTYPE));
-    cudaMemcpy(cuInput, neuron_i, CONV1_NEURON_INPUT_SIZE*sizeof(VTYPE), cudaMemcpyHostToDevice);
-    cudaMemcpy(cuKernels, synapse, CONV1_SYNAPSE_SIZE*sizeof(VTYPE), cudaMemcpyHostToDevice);
+    dim3 gridDim(CONV1_NXSCL, CONV1_NYSCL, CONV1_Nn);
+    dim3 blockDim(16, 8, 8);
 
-    convolution_layer_tiled_gpu<<<CONV1_BLOCKS, CONV1_THREADS>>>(*synapse_2, *neuron_i_2, *neuron_n);
+    float *cuInput, *cuKernels, *cuOutput;
+    CHECK_CUDA_ERROR(cudaMalloc(&cuInput, CONV1_NEURON_INPUT_SIZE*sizeof(VTYPE)));
+    CHECK_CUDA_ERROR(cudaMalloc(&cuKernels, CONV1_SYNAPSE_SIZE*sizeof(VTYPE)));
+    CHECK_CUDA_ERROR(cudaMalloc(&cuOutput, CONV1_NEURON_OUTPUT_SIZE*sizeof(VTYPE)));
+    CHECK_CUDA_ERROR(cudaMemcpy(cuInput, neuron_i_2, CONV1_NEURON_INPUT_SIZE*sizeof(VTYPE), cudaMemcpyHostToDevice));
+    CHECK_CUDA_ERROR(cudaMemcpy(cuKernels, synapse_2, CONV1_SYNAPSE_SIZE*sizeof(VTYPE), cudaMemcpyHostToDevice));
+
+    convolution_layer_tiled_gpu<<<gridDim, blockDim>>>(cuKernels, cuInput, cuOutput);
+
+    CHECK_CUDA_ERROR(cudaMemcpy(neuron_n, cuOutput, CONV1_NEURON_OUTPUT_SIZE*sizeof(VTYPE), cudaMemcpyDeviceToHost));
+
+    cudaFree(cuOutput);
+    cudaFree(cuInput);
+    cudaFree(cuKernels);
+
+    free(synapse_2);
+    free(neuron_i_2);
+    free(neuron_n);
+
+    CHECK_LAST_CUDA_ERROR();
+    cudaDeviceReset();
 }
 
 
