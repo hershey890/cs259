9d8f98a34c1f656006b93aebf02c9854325502b5
Hersh Joshi <hersh.joshi1@gmail.com>
2023-05-01 10:41:10 -0700

    unsafe commit. everything compiles and runs but is logically invalid

--- project1/convolution.cu
+++ project1/convolution.cu
@@ -40,86 +40,15 @@ const int nBlocks = 500; // Titan V has 640 cores and 80 SM
 const int nThreads = 1024; // divisible by 32, max 1024
 
 
-void conv2d_cpu(float *input, float *kernels, float *output)
+/*
+ * Run convolution using cuDNN
+ * @param input: input tensor NCHW (# outputs, # input channels, height, width)
+ * @param kernels: kernel tensor NCHW (# outputs, # input channels, height, width)
+ * @param output: output tensor NCHW (# outputs, # input channels, height, width). 
+ *  Output is assumed to be preallocated and is overwritten.
+ */
+void runCUDNNConv(float *input, float *kernels, float *output)
 {
-    const int startX = Kx / 2;
-    const int startY = Ky / 2;
-    const int endX = Nx - Kx / 2;
-    const int endY = Ny - Ky / 2;
-
-    for(int i=0; i<Nn; i++) {
-        for(int j=0; j<Ni; j++) {
-            for(int k=startY; k<endY; k++) {
-                for(int l=startX; l<endX; l++) {
-                    
-                    float sum = 0;
-                    for(int y=-Ky/2; y<=Ky/2; y++) {
-                        for(int x=-Kx/2; x<=Kx/2; x++)
-                            sum += kernels[i*Ni*Kx*Ky + j*Kx*Ky + y*Kx + x]*input[j*Nx*Ny + (k+y)*Nx + (l+x)];
-                    }
-                    output[i*outNx*outNy + (k-startY)*outNx + (l-startX)] = sum;
-
-                }
-            }
-        }
-    }
-}
-
-
-// __global __ void conv2d(float *input, float *kernel, float *output)
-// {
-//     const int startX = Kx / 2;
-//     const int startY = Ky / 2;
-//     const int endX = Nx - Kx / 2;
-//     const int endY = Ny - Ky / 2;
-
-//     const int i = blockIdx.x * blockDim.x + threadIdx.x;
-//     const int j = blockIdx.y * blockDim.y + threadIdx.y;
-//     const int k = blockIdx.z * blockDim.z + threadIdx.z;
-
-//     if(i < Ni && j < outNy && k < outNx) {
-//         float sum = 0;
-//         for(int y=-Ky/2; y<=Ky/2; y++) {
-//             for(int x=-Kx/2; x<=Kx/2; x++)
-//                 sum += kernel[y*Kx + x]*input[i*Nx*Ny + (j+y)*Nx + (k+x)];
-//         }
-//         output[j*outNx + k] = sum;
-//     }
-// }
-__global__ void conv2d_gpu(float *input, float *kernel, float *output)
-{
-    const int startX = Kx / 2;
-    const int startY = Ky / 2;
-    const int endX = Nx - Kx / 2;
-    const int endY = Ny - Ky / 2;
-
-    for(int i=0; i<Ni; i++) {
-        for(int j=startY; j<endY; j++) {
-            for(int k=startX; k<endX; k++) {
-                float sum = 0;
-                for(int y=-Ky/2; y<=Ky/2; y++) {
-                    for(int x=-Kx/2; x<=Kx/2; x++)
-                        sum += kernel[y*Kx + x]*input[i*Nx*Ny + (j+y)*Nx + (k+x)];
-                }
-                output[(j-startY)*outNx + (k-startX)] = sum;
-            }
-        }
-    }
-}
-
-
-int main(void)
-{
-    // Create Image, Kernel, and Output
-    float *input = (float*)malloc(Ni*Nx*Ny*sizeof(float));
-    float *kernels   = (float*)malloc(Nn*Ni*Kx*Ky*sizeof(float));
-    float *output  = (float*)malloc(Nn*outNx*outNy*sizeof(float));
-    for(int i=0; i<Nx*Ny; i++)
-        input[i] = (float)rand() / (float)RAND_MAX;
-    for(int i=0; i<Nn*Kx*Ky; i++)
-        kernels[i] = (float)rand() / (float)RAND_MAX;
-
-    // CUDNN Benchmark
     cudnnHandle_t cudnn;
     cudnnCreate(&cudnn);
 
@@ -136,37 +65,43 @@ int main(void)
     cudnnSetFilter4dDescriptor(kernelDesc, CUDNN_DATA_FLOAT, CUDNN_TENSOR_NCHW, Nn, Ni, Ky, Kx);
     cudnnSetTensor4dDescriptor(outputDesc, CUDNN_TENSOR_NCHW, CUDNN_DATA_FLOAT, 1, Nn, outNy, outNx);
     cudnnSetConvolution2dDescriptor(convDesc, 0, 0, 1, 1, 1, 1, CUDNN_CROSS_CORRELATION, CUDNN_DATA_FLOAT);
+
+    float *cuInput, *cuKernels, *cuOutput;
+    cudaMalloc(&cuInput, Ni*Nx*Ny*sizeof(float));
+    cudaMalloc(&cuKernels, Nn*Ni*Kx*Ky*sizeof(float));
+    cudaMalloc(&cuOutput, Nn*outNx*outNy*sizeof(float));
+    cudaMemcpy(cuInput, input, Ni*Nx*Ny*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(cuKernels, kernels, Nn*Ni*Kx*Ky*sizeof(float), cudaMemcpyHostToDevice);
+    float alpha = 1.0f, beta = 0.0f;
+    cudnnConvolutionForward(cudnn, &alpha, inputDesc, cuInput, kernelDesc, cuKernels, convDesc, CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM, NULL, 0, &beta, outputDesc, cuOutput);
+    cudaMemcpy(output, cuOutput, Nn*outNx*outNy*sizeof(float), cudaMemcpyDeviceToHost);
     
-    // cudnnGetConvolution2dForwardOutputDim(convDesc, inputDesc, kernelDesc, &Nn, &outNx, &outNy);
-    cudnnConvolutionFwdAlgoPerf_t perfResults[1];
-    int returnedAlgoCount = 1;
-    cudaProfilerStart();
-    CHECK_CUDNN(cudnnFindConvolutionForwardAlgorithm(cudnn, inputDesc, kernelDesc, convDesc, outputDesc, 1, &returnedAlgoCount, perfResults));
-    cudaProfilerStop();
+    cudaFree(cuInput);
+    cudaFree(cuKernels);
+    cudaFree(cuOutput);
+    
+}
 
 
-    // std::cout << perfResults[0] << std::endl;
-    // float *cuInput, *cuKernels, *cuOutput;
-    // cudaMalloc(&cuInput, Ni*Nx*Ny*sizeof(float));
-    // cudaMalloc(&cuKernels, Nn*Ni*Kx*Ky*sizeof(float));
-    // cudaMalloc(&cuOutput, Nn*outNx*outNy*sizeof(float));
-    // cudaMemcpy(cuInput, input, Ni*Nx*Ny*sizeof(float), cudaMemcpyHostToDevice);
-    // cudaMemcpy(cuKernels, kernels, Nn*Ni*Kx*Ky*sizeof(float), cudaMemcpyHostToDevice);
-    // float alpha = 1.0f, beta = 0.0f;
-    // cudnnConvolutionForward(cudnn, &alpha, inputDesc, cuInput, kernelDesc, cuKernels, convDesc, CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM, NULL, 0, &beta, outputDesc, cuOutput);
-    // cudaMemcpy(output, cuOutput, Nn*outNx*outNy*sizeof(float), cudaMemcpyDeviceToHost);
+__global__ void Conv2dGpu(float *input, float *kernels, float *output)
+{
+    ;
+}
 
-    // // Naive CPU Implementation
-    // double elapsedTime = 0;
-    // for(int i=0; i<nIters; i++) {
-    //     auto time0 = std::chrono::steady_clock::now();
-    //     conv2d_cpu(input, kernels, output);
-    //     auto time1 = std::chrono::steady_clock::now();
-    //     std::chrono::duration<double> elapsedSeconds = time1 - time0;
-    //     elapsedTime += elapsedSeconds.count();
-    // }
-    // std::cout << "CPU Time: " << elapsedTime/nIters << std::endl;
-    // arrayToFile<float>(output, Nn*outNx*outNy, "output_cpu.txt");
+
+int main(void)
+{
+    // Create Image, Kernel, and Output
+    float *input = (float*)malloc(Ni*Nx*Ny*sizeof(float));
+    float *kernels   = (float*)malloc(Nn*Ni*Kx*Ky*sizeof(float));
+    float *output  = (float*)malloc(Nn*outNx*outNy*sizeof(float));
+    for(int i=0; i<Nx*Ny; i++)
+        input[i] = (float)rand() / (float)RAND_MAX;
+    for(int i=0; i<Nn*Kx*Ky; i++)
+        kernels[i] = (float)rand() / (float)RAND_MAX;
+
+    // CUDNN Benchmark
+    runCUDNNConv(input, kernels, output);
 
     // GPU Implementation
     float *cuInput, *cuKernels, *cuOutput;
@@ -175,16 +110,7 @@ int main(void)
     cudaMalloc(&cuOutput, Nn*outNx*outNy*sizeof(float));
     cudaMemcpy(cuInput, input, Ni*Nx*Ny*sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(cuKernels, kernels, Nn*Ni*Kx*Ky*sizeof(float), cudaMemcpyHostToDevice);
-
-    elapsedTime = 0;
-    for(int i=0; i<nIters; i++) {
-        auto time0 = std::chrono::steady_clock::now();
-        conv2d<<<nBlocks, nThreads>>>(cuInput, cuKernel, cuOutput);
-        cudaDeviceSynchronize();
-        auto time1 = std::chrono::steady_clock::now();
-        std::chrono::duration<double> elapsedSeconds = time1 - time0;
-        elapsedTime += elapsedSeconds.count();
-    }
+    Conv2dGpu<<<nBlocks, nThreads>>>(cuInput, cuKernels, cuOutput);
 
     // Free Memory
     free(output);
