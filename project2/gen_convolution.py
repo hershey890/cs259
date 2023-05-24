df0aa38f82269e51f3c76e7f76fd801d6fe41fa7
Ricky Guo rickyrguo@g.ucla.edu <eriadnus@tetracosa.cs.ucla.edu>
2023-05-23 18:24:19 -0700

    server down for ricky, temp commit

--- project2/gen_convolution.py
+++ project2/gen_convolution.py
@@ -20,7 +20,19 @@ NUM_STREAMING_MULTIPROCESSORS = 80
 SINGLE_PRECISION_THROUGHPUT = 14.03
 SINGLE_PRECISION_THROUGHPUT_PER_SM = SINGLE_PRECISION_THROUGHPUT / NUM_STREAMING_MULTIPROCESSORS
 THREADS_PER_BLOCK = 1024
+OPS_PER_MAC = 2
 
+
+def get_operation_intensity(kx, ky, nx, ny, ni, nn, mem_size_bytes):
+    weights = kx * ky * ni * nn * DATATYPE_BYTES
+    inputs = nx * ny * ni * DATATYPE_BYTES
+    outputs = (nx - kx + 1) * (ny - ky + 1) * nn * DATATYPE_BYTES
+    total_size_bytes = weights + inputs + outputs
+    if total_size_bytes > mem_size_bytes:
+        return 0.0, False
+    computations = kx * ky * nx * ny * ni * nn
+    operation_intensity = computations / mem_size_bytes
+    return operation_intensity, True
 
 def get_execution_time(kx, ky, nx, ny, ni, nn, block_x_dim, block_y_dim, block_z_dim):
     blocks = ((nx - kx + 1) / block_x_dim) * ((ny - ky + 1) / block_y_dim) * (nn / block_z_dim)
@@ -50,18 +62,6 @@ def get_memory_load_time(kx, ky, nx, ny, ni, nn):
     return memory_load_time
 
 
-def get_operation_intensity(kx, ky, nx, ny, ni, nn, mem_size_bytes):
-    weights = kx * ky * ni * nn * DATATYPE_BYTES
-    inputs = nx * ny * ni * DATATYPE_BYTES
-    outputs = (nx - kx + 1) * (ny - ky + 1) * nn * DATATYPE_BYTES
-    total_size_bytes = weights + inputs + outputs
-    if total_size_bytes > mem_size_bytes:
-        return 0.0, False
-    computations = kx * ky * nx * ny * ni * nn
-    operation_intensity = computations / mem_size_bytes
-    return operation_intensity, True
-
-
 def create_and_run_convolution(nx, ny, kx, ky, ni, nn, block_x_dim, block_y_dim, block_z_dim):
     with open('_convolution.cu', 'r') as base_conv_fo:
         base_conv_file_str = base_conv_fo.read()
@@ -80,15 +80,24 @@ def create_and_run_convolution(nx, ny, kx, ky, ni, nn, block_x_dim, block_y_dim,
             interpolated_conv_fo.write(interpolated_conv_file_str)
 
     subprocess.call(["make"])
-    nvprof_out = subprocess.check_output(["nvprof", "./convolution"], stderr=subprocess.STDOUT, universal_newlines=True)
-    # subprocess.call(["make", "clean"])
+    nvprof_runtime_out = subprocess.check_output(["nvprof", "./convolution"], stderr=subprocess.STDOUT, universal_newlines=True)
+    nvprof_operations_out = subprocess.check_output(["nvprof", "--kernels", "Conv2dGpu", "--metrics", "flop_count_sp", "./convolution"], stderr=subprocess.STDOUT, universal_newlines=True)
+    subprocess.call(["make", "clean"])
     runtime_match = re.findall(
         '\\d+[.]?\\d+%\\s+\\d+[.]?\\d+(?: ns|us|ms)\\s+\\d+\\s+\\d+[.]?\\d+(?:ns|us|ms)\\s+\\d+[.]?\\d+(?:ns|us|ms)\\s+'
-        '(\\d+[.]?\\d+)(ns|us|ms)\\s+Conv2dGpu[(]float[*], float[*], float[*][)]', nvprof_out)
+        '(\\d+[.]?\\d+)(ns|us|ms)\\s+Conv2dGpu[(]float[*], float[*], float[*][)]', nvprof_runtime_out)
+    operations_match = re.findall(
+        '\\d+\\s+flop_count_sp\\s+Floating Point Operations[(]Single Precision[)]\\s+(\\d+)', nvprof_operations_out
+    )
+    if 'CUDA Runtime Error' in nvprof_runtime_out or 'CUDA Runtime Error' in nvprof_operations_out:
+        return
     if len(runtime_match) != 1:
-        print(nvprof_out)
-        raise 'unknown match discovered'
+        # print(nvprof_runtime_out)
+        raise ValueError(nvprof_operations_out)
+    if len(operations_match) != 1:
+        raise ValueError(nvprof_operations_out)
     runtime_decimal, runtime_unit = runtime_match[0]
+    operations = operations_match[0]
     runtime = float(runtime_decimal)
     if runtime_unit == 'ns':
         runtime *= 1e-9
@@ -96,8 +105,35 @@ def create_and_run_convolution(nx, ny, kx, ky, ni, nn, block_x_dim, block_y_dim,
         runtime *= 1e-6
     else:
         runtime *= 1e-3
-    # print('Runtime in seconds: {}'.format(runtime))
-    # print('TeraOPs/s: {}'.format((computations / 1e12) / runtime))
+    return runtime, int(operations)
+
+
+def compare_model(nx, ny, kx, ky, ni, nn, block_x_dim, block_y_dim, block_z_dim):
+    weights = kx * ky * ni * nn * DATATYPE_BYTES
+    inputs = kx * ny * ni * DATATYPE_BYTES
+    outputs = (kx - kx + 1) * (ny - ky + 1) * nn * DATATYPE_BYTES
+    total_size_bytes = weights + inputs + outputs
+
+    # nvprof results\
+    try:
+        runtime_seconds_nvprof, operations = create_and_run_convolution(nx, ny, kx, ky, ni, nn, block_x_dim, block_y_dim, block_z_dim)
+    except ValueError as e:
+        print(e)
+        return
+    operational_intensity_nvprof = operations / total_size_bytes / OPS_PER_MAC
+    teraops_nvprof = (operations * OPS_IN_TERA_OPS) / runtime_seconds_nvprof
+
+    # model results
+    operation_intensity_model, can_fit_l1 = get_operation_intensity(kx, ky, nx, ny, ni, nn, total_size_bytes)
+    execution_time = get_execution_time(kx, ky, nx, ny, ni, nn, block_x_dim, block_y_dim, block_z_dim)
+    memory_load_time = get_memory_load_time(kx, ky, nx, ny, ni, nn)
+    runtime_seconds_model = max(execution_time, memory_load_time)
+    computations = kx * ky * nx * ny * ni * nn
+    teraops_model = computations * OPS_IN_TERA_OPS / runtime_seconds_model
+
+    print("Operational intensity: ", operational_intensity_nvprof, operation_intensity_model)
+    print("TOP/s:                 ", teraops_nvprof, teraops_model)
+    print("Runtime (us):          ", runtime_seconds_nvprof*1e6, runtime_seconds_model*1e6)
 
 
 def main():
@@ -123,28 +159,8 @@ def main():
     block_x_dim = args.blockXDim
     block_y_dim = args.blockYDim
     block_z_dim = args.blockZDim
-    print('{} us'.format(get_execution_time(kx, ky, nx, ny, ni, nn, block_x_dim, block_y_dim, block_z_dim) * 1e6))
-    create_and_run_convolution(nx, ny, kx, ky, ni, nn, block_x_dim, block_y_dim, block_z_dim)
-    # print('{} us'.format(get_memory_load_time(kx, ky, nx, ny, ni, nn)))
 
-    # weights = args.Kx * args.Ky * args.Ni * args.Nn * DATATYPE_BYTES
-    # inputs = args.Nx * args.Ny * args.Ni * DATATYPE_BYTES
-    # outputs = (args.Nx - args.Kx + 1) * (args.Ny - args.Ky + 1) * args.Nn * DATATYPE_BYTES
-    # total_size_bytes = weights + inputs + outputs
-    # decimal_l1 = total_size_bytes / (L1_CACHE_SIZE_KB * KIB_IN_BYTES)
-    # computations = args.Kx * args.Ky * args.Nx * args.Ny * args.Ni * args.Nn
-    # l1_operation_intensity = computations / total_size_bytes
-    # l2_tflops = l1_operation_intensity * OPS_IN_TERA_OPS * L2_BANDWIDTH_GBPS * KIB_IN_BYTES ** 3
-    # dram_tflops = l1_operation_intensity * OPS_IN_TERA_OPS * DRAM_BANDWIDTH_GBPS * KIB_IN_BYTES ** 3
-    #
-    # print('Weight space occupied in bytes: {}'.format(weights))
-    # print('Inputs space occupied in bytes: {}'.format(inputs))
-    # print('Outputs space occupied in bytes: {}'.format(outputs))
-    # print('Total size space occupied in bytes: {}'.format(total_size_bytes))
-    # print('Percent L1: {0:.2f}'.format(decimal_l1 * 100))
-    # print('Operation intensity: {0:.2f}'.format(l1_operation_intensity))
-    # print('L2 TFlops: {0:.2f}'.format(l2_tflops))
-    # print('DRAM TFlops: {0:.2f}'.format(dram_tflops))
+    compare_model(nx, ny, kx, ky, ni, nn, block_x_dim, block_y_dim, block_z_dim)
 
 
 if __name__ == '__main__':
