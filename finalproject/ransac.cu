76d3cdb07f5b48900adc1440118ae1c05b0cf171
eriadnus <rickyrguo@g.ucla.edu>
2023-06-07 11:25:30 -0700

    cleanup ransac.cu

--- finalproject/ransac.cu
+++ finalproject/ransac.cu
@@ -1,30 +1,31 @@
 #include <iostream>
 #include <fstream>
 
-struct SourceDestinationPointsBytes {
+struct Points {
 public:
-    float* src_pts_bytes; // n/2 rows x 2 cols
-    float* dst_pts_bytes; // n/2 rows x 2 cols
+    float* source; // n/2 rows x 2 cols
+    float* destination; // n/2 rows x 2 cols
 };
 
 // Read src_dst_pts.bin -
-SourceDestinationPointsBytes* readPtsFile(std::string filename)
-{
-    std::ifstream stream (filename, std::ios::in | std::ios::binary);
+Points* readPtsFile(std::string filename) {
+    std::ifstream stream(filename, std::ios::in | std::ios::binary);
 
     uint32_t n_bytes;
-    stream.read(reinterpret_cast<char*>(&n_bytes), sizeof(uint32_t));
+    stream.read(reinterpret_cast<char *>(&n_bytes), sizeof(uint32_t));
 
-    SourceDestinationPointsBytes* bytes = new (std::nothrow) SourceDestinationPointsBytes;
-    bytes->src_pts_bytes = new (std::nothrow) float[n_bytes / sizeof(float)];
-    bytes->dst_pts_bytes = new (std::nothrow) float[n_bytes / sizeof(float)];
-    stream.read(reinterpret_cast<char*>(bytes->src_pts_bytes), n_bytes);
-    stream.read(reinterpret_cast<char*>(bytes->dst_pts_bytes), n_bytes);
+    Points *points = new(std::nothrow) Points;
+    points->source = new(std::nothrow) float[n_bytes / sizeof(float)];
+    points->destination = new(std::nothrow) float[n_bytes / sizeof(float)];
+    stream.read(reinterpret_cast<char *>(points->source), n_bytes);
+    stream.read(reinterpret_cast<char *>(points->destination), n_bytes);
 
     stream.close();
 
-    return bytes;
+    return points;
 }
+
+
 
 // make 2 modes, 1 for a file and another for stdin
 float** read_stdin()
